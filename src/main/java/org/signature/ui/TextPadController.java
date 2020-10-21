d8d39e458b0523689db30ef19340d66bbe07a9ec
Atul Suryvanshi <48992673+Atul-2001@users.noreply.github.com>
2020-10-21 23:56:36 +0530

    #bug fixes

--- src/main/java/org/signature/ui/TextPadController.java
+++ src/main/java/org/signature/ui/TextPadController.java
@@ -334,6 +334,15 @@ public class TextPadController {
             File source = openFileDialog.showOpenDialog(window);
 
             if (source != null) {
+                if (new TextFile(source).getSizeInMB() > 10) {
+                    Alert fileSizeAlert = new Alert(Alert.AlertType.INFORMATION);
+                    fileSizeAlert.setTitle("Notepad");
+                    fileSizeAlert.setHeaderText(null);
+                    fileSizeAlert.setContentText("File size is too large to open\nin this text editor.");
+                    fileSizeAlert.showAndWait();
+                    return;
+                }
+                
                 textFile = new TextFile(source);
                 windowTitle.set(source.getName());
                 isEdited.set(false);
